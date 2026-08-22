423d1aedcff46b5338a226e47b25e77655f238f1
d0nya83 <donyabazrafshon1234@gmail.com>
2026-08-22 16:16:58 +0330

    add sum,minus,multiplication,division

---
+++ CalculatorApi/Controllers/CalculationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculationController : ControllerBase
+    {
+        [HttpPost("sum")]
+        public decimal sum(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        [HttpPost("minus")]
+        public decimal minus(decimal num1, decimal num2)
+        {
+            return num1 - num2;
+        }
+        [HttpPost("multiplication")]
+        public decimal multiplication(decimal num1, decimal num2)
+        {
+            return num1 * num2;
+
+        }
+        [HttpPost("division")]
+        public decimal division(decimal num1, decimal num2)
+        {
+            if (num2 == 0M)
+            {
+                return 0;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+
+        }
+    }
+}
